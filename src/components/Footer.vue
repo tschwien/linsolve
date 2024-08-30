97e44d147b39728eab3f21fe727bc247168991d2
Hannah-Frehe <frehe.hannah@gmail.com>
2024-08-30 16:59:46 +0200

    Added Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<script>
+    export default{
+        name:'Footer',
+    }
+</script>
+<template>
+    <footer class="footer">
+        <a class="footer-link">Dokumentation</a>
+        <a class="footer-link">Über uns</a>
+        <a class="footer-link">AGB</a>
+    </footer>
+</template>
+<style>
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.footer{
+    position: fixed;         
+    bottom: 0;                 
+    left: 0;                
+    width: 100%;            
+    display: flex;
+    background-color:  rgba(7, 7, 152, 0.945) ;
+    color: white;
+    justify-content: center;
+    align-items: center;
+    }
+.footer-link{
+    padding: 1rem;
+    color: white;
+}
+
+</style>
