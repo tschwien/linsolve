0865bd13ba8c69450c5631a455c82533e170396a
Hannah-Frehe <frehe.hannah@gmail.com>
2024-09-10 12:21:08 +0200

    Added multi language support to the About Us view

--- src/components/About_us.vue
+++ src/components/About_us.vue
@@ -6,25 +6,19 @@ export default{
 <template>
     <div>
        <h1>
-            Über uns – Das LinSolve-Team
+           {{ $t('aboutUsHeadline') }}
         </h1>
-        <p>    
-
-Hi! Wir sind Hannah, Jona, Tom und Nele, ein Team aus Wirtschaftsinformatik-Studierenden der Hochschule Lingen im 5. Semester. Im Rahmen unseres Programmierprojekts haben wir LinSolve entwickelt – die Plattform, die euch bei mathematischen Problemen im Bereich Operations Research (OR) unter die Arme greift.
-
-Unsere Mission? OR-Probleme schnell und einfach lösen – ohne komplizierte Tools. LinSolve hilft euch bei linearen Gleichungen, Optimierungsproblemen und mehr, und das auf verständliche Weise. 
-
-Entwickelt von Studierenden für Studierende, wollen wir den Studienalltag erleichtern. Testet LinSolve und gebt uns Feedback – wir freuen uns drauf!
-
-Euer LinSolve-Team 💻
+        <br>
+        <p>   {{$t('aboutUsText')}} 💻
         </p>
         <div class="Home">
+          <br>
           <button class="mainButton" @click="optimizationStore.addConstraint()">{{ $t('addConstraint') }}</button>
          
       </div>
     </div>
 </template>
-<style>
+<style scoped>
 p {
   font-size: 18px;
   color: #333;
