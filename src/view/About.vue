cfcd30abbe2b318fe8df1c563e6825839dba65cb
Hannah-Frehe <frehe.hannah@gmail.com>
2024-09-10 12:01:47 +0200

    Bugfixes in About

--- src/view/About.vue
+++ src/view/About.vue
@@ -5,13 +5,13 @@
     </template>
     <script>
     import Header from '../components/Header.vue';
-    import AboutUss from '../components/About_us.vue';
+    import AboutUs from '../components/About_us.vue';
     import Footer from '../components/Footer.vue';
     export default{
         components:{
             Header, 
             Footer, 
-            About_us
+            AboutUs
         }
     }
     </script>
